38f8c049219ad41cffd67322adb382e64180dedd
sujal <sujal448863@gmail.com>
2026-09-08 12:11:05 +0530

    Add OOPs package

---
+++ src/Pointer/twosum.java
@@ -0,0 +1,36 @@
+package Pointer;
+import java.util.Scanner;
+public class twosum {
+
+//    static boolean sum(int[] arr, int target) {
+//        int left = 0;
+//        int right = arr.length - 1;
+//        while (left < right) {
+//            int sum = arr[left] + arr[right];
+//            if (sum == target) {
+//                return true;
+//            } else if (sum < target) {
+//                left++;
+//            } else {
+//                right--;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = {-8, 1, 4, 6, 10, 45};
+//        Scanner sc = new Scanner(System.in);
+//        int target = sc.nextInt();
+//        if (sum(arr, target)) {
+//            System.out.println("true");
+//        }
+//        else{
+//            System.out.println("False");
+//        }
+//    }
+
+
+
+    
+}
