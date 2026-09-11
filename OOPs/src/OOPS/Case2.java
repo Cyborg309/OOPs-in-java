6997169398b268c900ae0b6dc4f7accb1fb24f49
sujal <sujal448863@gmail.com>
2026-09-11 12:17:42 +0530

    Updated Case2

---
+++ OOPs/src/OOPS/Case2.java
@@ -0,0 +1,53 @@
+package OOPS;
+import static java.lang.Math.*;
+public class Case2 {
+        public static void main(String[] args) {
+            Shape[] shapes = new Shape[2];
+            shapes[0] = new Circle(5.0);
+            shapes[1] = new Rectangle(4.0, 6.0);
+
+            for (Shape s : shapes) {
+                s.displayArea();
+            }
+        }
+    }
+abstract class Shape {
+
+    abstract double area();
+
+    void displayArea() {
+        System.out.println("Area = " + area());
+    }
+}
+
+class Circle extends Shape {
+
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    double length;
+    double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+}
+
+
